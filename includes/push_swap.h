aa156da0e70e07a8ff89e44b71f25710a80c2b7f
Watty Ballabar <wballaba@e2r3p4.21-school.ru>
2019-02-06 17:29:20 +0300

    sa ra rra

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,28 +6,50 @@
 /*   By: wballaba <wballaba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 17:57:07 by wballaba          #+#    #+#             */
-/*   Updated: 2019/02/05 16:53:24 by wballaba         ###   ########.fr       */
+/*   Updated: 2019/02/06 14:51:18 by wballaba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+# define	FROM_A_TO_B 0
+# define	FROM_B_TO_A 1
+# define	CALL_BOTH	2
 
 # include "libft.h"
 
+void 	ps_swap(t_stack *stack, int len, int op);
+void	ps_rotate(t_stack *stack, int len, int op);
+void	ps_reverse_rotate(t_stack *stack, int len, int op);
+
 typedef struct	s_cmd
 {
-	int				sa;
-	int				sb;
-	int				ss;
-	int				pa;
-	int				pb;
-	int				ra;
-	int				rb;
-	int				rr;
-	int				rra;
-	int				rrb;
-	int				rrr;
+	char		cmd[4];
+	void		(*f)(t_stack *stack, int len, int op);
+	char		type_args;
 }				t_cmd;
+
+typedef struct	s_stack
+{
+	int	*a;
+	int *b;
+}				t_stack;
+
+t_cmd	g_ps_cmds[] = {
+	{ "sa", ps_swap,	FROM_A_TO_B },
+	// { "sb", ps_swap,	FROM_B_TO_A },
+	// { "ss", ps_swap,	CALL_BOTH },
+	// { "pa", ps_push,	FROM_B_TO_A },
+	// { "pb", ps_push,	FROM_A_TO_B },
+	{ "ra", ps_rotate,	FROM_A_TO_B },
+	// { "rb", ps_rotate,	FROM_B_TO_A },
+	// { "rr", ps_rotate,	CALL_BOTH },
+	{ "rra", ps_reverse_rotate, FROM_A_TO_B },
+	// { "rrb", ps_reverse_rotate, FROM_B_TO_A },
+	// { "rrr", ps_reverse_rotate, CALL_BOTH },
+	{ "", NULL, 0 }
+};
+
+
 
 #endif
--- srcs/checker.c
+++ srcs/checker.c
@@ -6,39 +6,145 @@
 /*   By: wballaba <wballaba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 17:55:07 by wballaba          #+#    #+#             */
-/*   Updated: 2019/02/05 17:05:08 by wballaba         ###   ########.fr       */
+/*   Updated: 2019/02/06 14:58:47 by wballaba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int argc, char **argv)
+
+
+void	ps_reverse_rotate(t_stack *stack, int len, int op)
 {
-	char	*line;
-	char	**vals;
-	int		i;
-	int		j;
+	int	tmp[len - 1];
+	int	vals[len - 1];
+	int i;
+	int	j;
 
 	i = 0;
-	j = 1;
-	if (!(vals = (char **)malloc(sizeof(vals) * (argc))))
-		return (0);
-	while (j < argc)
+	j = 0;
+	// if (g_ps_cmds[op].type_args == 0)
+
+	tmp[j] = vals[len - 1];
+	j++;
+	while (j < len)
 	{
-		vals[i] = ft_strdup(argv[j]);
-		// ft_putendl(vals[i]);
-		ft_putendl(vals[i]);
+		tmp[j] = vals[i];
 		i++;
 		j++;
 	}
-	vals[i] = NULL;
+	i = 0;
+	while (i < len)
+	{
+		vals[i] = tmp[i];
+		i++;
+	}
+}
+
+void	ps_rotate(t_stack *stack, int len, int op)
+{
+	int	tmp[len - 1];
+	int i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	tmp[len - 1] = vals[i];
+	i++;
+	while (i < len)
+	{
+		tmp[j] = vals[i];
+		i++;
+		j++;
+	}
+	i = 0;
+	while (i < len)
+	{
+		vals[i] = tmp[i];
+		i++;
+	}
+}
+
+void	ps_swap(t_stack *stack, int len, int op)
+{
+	if (g_ps_cmds[op].type_args == 0)
+		ft_swap(&stack->a[0], &stack->a[1]);
+	else if (g_ps_cmds[op].type_args == 1)
+		ft_swap(&stack->b[0], &stack->b[1]);
+	else
+	{
+		ft_swap(&stack->a[0], &stack->a[1]);
+		ft_swap(&stack->b[0], &stack->b[1]);
+	}
+}
+
+int		check_vals(t_stack *stack, int len)
+{
+	int i;
+
+	i = 0;
+	while (i <= len)
+	{
+		if (i < len)
+		{
+			if (stack->a[i] > stack->a[i + 1])
+			{
+				write(1, "KO\n", 3);
+				return (0);
+			}
+		}
+		else
+		{
+			write(1, "OK\n", 3);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int		read_instruchtions(t_stack *stack, int len)
+{
+	char	*line;
+	int		i;
+
 	while (get_next_line(0, &line) == 1)
 	{
-		ft_putendl(line);
-		// *str[i] = *line;
-		// i++;
-		
-		// free(line);
+		i = 0;
+		while (i < 11)
+		{
+			if (!ft_strcmp(line, g_ps_cmds[i].cmd))
+				g_ps_cmds[i].f(stack, len);
+			i++;
+		}
 	}
+	return (1);
+}
+
+int main(int argc, char **argv)
+{
+	int		vals[argc - 1];
+	int		i;
+	int		j;
+	t_stack	*stack;
+
+	i = 0;
+	j = 1;
+	while (j < argc)
+	{
+		stack->a[i] = ft_atoi(argv[j]);
+		i++;
+		j++;
+	}
+	read_instruchtions(stack, i);
+	j = 0;
+	while (j < i)
+	{
+		ft_printf("%d ", vals[j]);
+		j++;
+	}
+	ft_printf("\n");
+	check_vals(vals, i);
+
 	return (0);
 }
